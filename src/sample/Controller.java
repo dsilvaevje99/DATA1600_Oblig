8f7c641453198022a0b4674fcc1d88c9c9052ad2
Daniel Silva <dsilvaevje@gmail.com>
2020-02-13 13:59:02 +0100

    First functional software version

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,23 +1,79 @@
 package sample;
 
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
-public class Controller {
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+
+    DataCollection collection = new DataCollection();
+    PersonRegister register = new PersonRegister();
+    int index = 0;
+
+
     @FXML
     DatePicker birthday;
 
     @FXML
-    TextField FirstName, age, email, celNum, lastName;
+    TextField firstName, email, celNum, lastName;
+
+    @FXML
+    TableView tableView;
 
     @FXML
     TableColumn<DataModel, String> tableFirstName, tableLastName, tableAge, tableBirthday, tableEmail, tableCelNum;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        collection.linkTableView(tableView);
+    }
 
+    @FXML
+    private void addElementBtnClicked() {
+        register.createPerson(firstName.getText(), lastName.getText(), calculateAge(birthday), birthday.getValue(), email.getText(), celNum.getText());
+        System.out.println(register.getPerson(0).getfName());
+        DataModel obj = createDataModelObjectFromGUI();
 
+        if(obj != null) {
+            resetAllFields();
+            collection.addElement(obj);
+        }
+    }
+
+    private DataModel createDataModelObjectFromGUI() {
+        String firstNameData = register.getPerson(index).getfName();
+        String lastNameData = register.getPerson(index).getlName();
+        String birthdayData = register.getPerson(index).getbDay().toString();
+        String emailData = register.getPerson(index).getEmail();
+        String celNumData = register.getPerson(index).getCelNum();
+        int ageData = register.getPerson(index).getAge();
+        index++;
+        try {
+            return new DataModel(firstNameData, lastNameData, ageData, birthdayData, emailData, celNumData);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+
+    }
+
+    private void resetAllFields() {
+        firstName.setText("");
+        lastName.setText("");
+        birthday.getEditor().clear();
+        email.setText("");
+        celNum.setText("");
+    }
+
+    private int calculateAge(DatePicker birthday) {
+        LocalDate now = LocalDate.now();
+        Period diff = Period.between(birthday.getValue(), now);
+        return diff.getYears();
+    }
 
 }
--- src/sample/PersonRegister.java
+++ src/sample/PersonRegister.java
@@ -1,5 +1,19 @@
 package sample;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class PersonRegister {
+
+    ArrayList<Person> personArrayList = new ArrayList<>();
+
+    public void createPerson(String fName, String lName, int age, LocalDate bDay, String email, String celNum) {
+        Person person = new Person(fName, lName, age, bDay, email, celNum);
+        personArrayList.add(person);
+    }
+
+    public Person getPerson(int index) {
+        return personArrayList.get(index);
+    }
 
 }
